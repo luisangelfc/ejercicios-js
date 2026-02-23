b035152aeca9b670b3f51a0ec60469fdb8eef676
bubu-laptop <luisangel@ciencias.unam.mx>
2026-02-23 08:49:20 -0600

    agregando primeros ejercios de logica en JS

---
+++ par-o-impar.js
@@ -0,0 +1,15 @@
+document.write("programa que detecta numeros pares e impares" + "<br>");
+
+let x = parseInt(prompt("escribe el numero"))
+document.writeln("escogiste el numero:" + x + "<br>");
+    if (!isNaN(x)){
+            if (x % 2 == 0){
+        document.writeln("es par")
+        }
+        else {
+        document.writeln("es impar")
+
+        }
+    } else {
+        document.writeln("Ingresa un numero valido")
+    }
---
+++ variables.js
@@ -0,0 +1,25 @@
+let valor= true;
+let valor2= false;
+
+let resultado= valor && valor2;
+
+document.write(resultado);
+
+let num1=23;
+let num2=1;
+
+let resultadoo= num1 != num2;
+document.writeln(resultadoo); 
+
+
+const x = 38;
+const y = 73;
+const z = 23;
+const w = 14;
+const a = 97;
+
+
+let op= (x < y) || (w >= a) && !(z<x && )
+
+
+
