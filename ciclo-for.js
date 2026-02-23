e0e854c8dc1d3da330fecd2ae152a8837633fbf7
bubu-laptop <luisangel@ciencias.unam.mx>
2026-02-23 13:15:24 -0600

    2do ejercicio con for

--- ciclo-for.js
+++ ciclo-for.js
@@ -7,6 +7,19 @@ function detectarParoImpar (x){
 }
 
 //utilizando for
-for(x=1;x<11;x++){
-    console.log(x + detectarParoImpar(x) );
+// for(x=1;x<11;x++){
+//     console.log(x + detectarParoImpar(x) );
+// }
+
+
+//reto ciclo for
+let suma=0;
+for(x=1;x<21;x++){
+    if(detectarParoImpar(x)== " es par"){
+        console.log(x);
+        suma= suma+x;
+
+    }     
 }
+
+    console.log(suma);
