3031e34b2e2df37ace8838ca571734c75e6e472b
bubu-laptop <luisangel@ciencias.unam.mx>
2026-02-23 12:22:10 -0600

    revisando tipos de datos

---
+++ Tipos-variables.js
@@ -0,0 +1,14 @@
+// Simularemos una app 
+const nombre= "bubuloco";
+let numero="945899393";
+const correo= "correo@exapmle.com";
+const direccion="cdmx";
+let edad= 34;
+let isActivite= true;
+
+console.log(typeof nombre);
+console.log(typeof numero);
+console.log(typeof correo);
+console.log(typeof direccion);
+console.log(typeof edad);
+console.log(typeof isActivite);
