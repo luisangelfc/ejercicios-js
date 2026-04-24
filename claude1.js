f028ca523918c88639d69c3c385e1f0f1b05ffcb
bubu-laptop <luisangel@ciencias.unam.mx>
2026-04-24 02:50:15 -0600

    agregando ejercicio de claude, conversion de pesos a dolares

---
+++ claude1.js
@@ -0,0 +1,8 @@
+const conversionPesosaDolares=(precio) => {
+  let conversion=precio/17
+  return conversion;
+}
+
+console.log(conversionPesosaDolares(400));
+console.log(conversionPesosaDolares(100));
+console.log(conversionPesosaDolares(900));
