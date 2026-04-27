40b7b3e4cc5db2fffb637a44f59d0b3d2bc90bf7
bubu-laptop <luisangel@ciencias.unam.mx>
2026-04-27 07:57:32 -0600

    optimizando la funcion de claude1.js

--- claude1.js
+++ claude1.js
@@ -1,7 +1,9 @@
 const conversionPesosaDolares=(precio) => {
   let conversion=precio/17
-  return conversion;
+  return conversion.toFixed(2);
 }
+
+// const conversionPesosaDolares = (precio) => (precio / 17).toFixed(2)
 
 console.log(conversionPesosaDolares(400));
 console.log(conversionPesosaDolares(100));
